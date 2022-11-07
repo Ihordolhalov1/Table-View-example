69e475a2a3f188b3fe81980e3864191d83f57358
Ihordolhalov1 <servicegambro@gmail.com>
2022-11-07 10:51:29 +0000

    Initial Commit

---
+++ Table View example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table View example
+//
+//  Created by Ihor Dolhalov on 07.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
